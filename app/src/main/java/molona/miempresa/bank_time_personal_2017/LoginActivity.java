838700225201584060d0548820f82212c33ea5b8
xusaresg <xusaresg@gmail.com>
2017-10-08 20:07:23 +0200

    Primera versión

---
+++ app/src/main/java/molona/miempresa/bank_time_personal_2017/LoginActivity.java
@@ -0,0 +1,13 @@
+package molona.miempresa.bank_time_personal_2017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/molona/miempresa/bank_time_personal_2017/MainActivityCovert.java
@@ -0,0 +1,18 @@
+package molona.miempresa.bank_time_personal_2017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivityCovert extends AppCompatActivity {
+    private Button btnGo ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_covert);
+    }
+
+
+
+}
